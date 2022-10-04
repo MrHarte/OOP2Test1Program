50c651f8b40b34476f3a8d223f8ce1ea72c23dc6
Matthias Harte <mharte@crcmail.net>
2022-10-04 17:50:27 -0400

    Add project files.

---
+++ OOP2Test1Program/Test1Form.cs
@@ -0,0 +1,16 @@
+namespace OOP2Test1Program
+{
+    /// <summary>
+    /// Form to start the tests.
+    /// </summary>
+    public partial class Test1Form : Form
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Test1Form"/> class.
+        /// </summary>
+        public Test1Form()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
